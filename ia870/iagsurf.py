761957e591e2f1269529008069633644ab30fdc7
Roberto A Lotufo <robertoalotufo@gmail.com>
2018-12-29 08:41:01 -0500

    surface view

--- ia870/iagsurf.py
+++ ia870/iagsurf.py
@@ -4,25 +4,21 @@
 def iagsurf(f, vx=1, vy=0, vz=1):
 
     import numpy as np
-    import ia636
-    zd = ia636.iapad(f).astype(float)
+    zd = np.pad(f,(1,1),'constant').astype(np.float)
     gv = zd[1:-1,2:] - zd[1:-1,1:-1]
     gv /= gv.max()
-    #adshow(ia636.ianormalize(gv))
     gh = zd[2:,1:-1] - zd[1:-1,1:-1]
     gh /= gh.max()
-    #adshow(ia636.ianormalize(gh))
     gz = 1.0/(gv**2 + gh**2 + 1)
-    #adshow(ia636.ianormalize(gz))
     gv *= gz
     gh *= gz
-    #adshow(ia636.ianormalize(gv))
-    #adshow(ia636.ianormalize(gh))
     v = np.sqrt(vx*vx + vy*vy + vz*vz)
-    vx = vx/v; vy = vy/v; vz = vz/v;
+    vx = vx/v; vy = vy/v; vz = vz/v
     gv *= vx
     gh *= vy
     gz *= vz
     gz += gh + gv
-    return ia636.ianormalize(gz)
+    m,M = gz.min(),gz.max()
+    gn = (255.*(gz - m)/(M-m)).astype(np.uint8)
+    return gn
 
